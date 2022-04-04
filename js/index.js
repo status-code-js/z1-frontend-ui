804e6bf7b910cdd50503b93a3e51f8dc4373e497
Sylwia <sonnenuntergang.email@gmail.com>
2022-04-04 03:32:45 +0200

    feat: add animations

---
+++ js/index.js
@@ -0,0 +1,21 @@
+
+window.addEventListener('scroll', animItems)
+
+function animItems () {
+    const anim = document.querySelectorAll('.animate')
+
+    for (let i = 0; i < anim.length; i++) {
+
+        const windowHeight = window.innerHeight
+        const showTop = anim[i].getBoundingClientRect().top
+        const animPoint = 150
+
+        if(showTop < windowHeight - animPoint) {
+            anim[i].classList.add('active')
+        } else {
+            if(!anim.classList.contains('no-hide'))
+            anim[i].classList.remove('active')
+        }
+
+    }
+}
